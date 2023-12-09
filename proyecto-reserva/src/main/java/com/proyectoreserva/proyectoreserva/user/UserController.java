983a17c031642374a4d50eca8b5c4cbc50974c30
Daniela <danielapb78@gmail.com>
2023-12-08 20:56:57 -0500

    implementando swagger

--- proyecto-reserva/src/main/java/com/proyectoreserva/proyectoreserva/user/UserController.java
+++ proyecto-reserva/src/main/java/com/proyectoreserva/proyectoreserva/user/UserController.java
@@ -1,5 +1,12 @@
 package com.proyectoreserva.proyectoreserva.user;
 
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -18,14 +25,36 @@ public class UserController {
         this.userService = userService;
     }
 
+   @Operation(summary = "Get a list of users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of users",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Users.class))),
+            @ApiResponse(responseCode = "404", description = "No users found",
+                    content = @Content),
+    })
+
+
     @GetMapping(path = "getUser")
     public List<Users> getUser() {
         return this.userService.getUser();
     }
 
     @PostMapping(path = "newUser")
-    public void newUser(@RequestBody Users user) {
-        this.userService.newUser(user);
+    public ResponseEntity<Object>  newUser(@RequestBody Users user) {
+       return this.userService.newUser(user);
     }
+
+    @PutMapping (path = "updateUser")
+    public ResponseEntity<Object>  updateUser(@RequestBody Users user) {
+        return this.userService.updateUser(user);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Object>  deleteUser(@PathVariable ("Id")  Integer id) {
+        return this.userService.deleteUser(id);
+    }
+
+
 
 }
--- proyecto-reserva/src/main/java/com/proyectoreserva/proyectoreserva/user/UserService.java
+++ proyecto-reserva/src/main/java/com/proyectoreserva/proyectoreserva/user/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    HashMap <String,Object> hashMap = new HashMap<>();
     @Autowired
     public UserService(UserRepository userRepository)
     {
@@ -19,24 +21,80 @@ public class UserService {
     }
 
     //Este es un constructor de tipo lista
-    public List<Users> getUser()
-    {
+    public List<Users> getUser() {
         return this.userRepository.findAll();
     }
+
     public ResponseEntity<Object> newUser(Users user) {
-      Optional <Users>  result =  userRepository.findUserByName(user.getName());
-        if (result.isPresent())
-        {
+        Optional<Users> result = userRepository.findUserByName(user.getName());
+
+
+        if (result.isPresent()) {
+            hashMap.put("Error", true);
+            hashMap.put("Message", "User already exists");
             return new ResponseEntity<>
-                    (
+                    (       hashMap,
                             HttpStatus.CONFLICT
                     );
             //throw new IllegalStateException("El usuario ya existe ");
         }
         userRepository.save(user);
+        hashMap.put("Info", user);
+        hashMap.put("Message", "User save");
+
         return new ResponseEntity<>(
+                hashMap,
                 HttpStatus.CREATED
         );
     }
-}
+
+    public ResponseEntity<Object> updateUser(Users user) {
+        Optional<Users> result = userRepository.findUserByName(user.getName());
+
+
+        if (result.isPresent() && user  .getId() == null) {
+            hashMap.put("Error", true);
+            hashMap.put("Message", "User already exists");
+            return new ResponseEntity<>
+                    (       hashMap,
+                            HttpStatus.CONFLICT
+                    );
+            //throw new IllegalStateException("El usuario ya existe ");
+        }
+        hashMap.put("Message", "User save");
+        if (user.getId() != null)
+        {
+            hashMap.put("message","user update");
+        }
+        userRepository.save(user);
+        hashMap.put("Info", user);
+
+        return new ResponseEntity<>(
+                hashMap,
+                HttpStatus.CREATED
+        );
+    }
+
+    public  ResponseEntity<Object> deleteUser(Integer id)
+    {
+      boolean existsId = this.userRepository.existsById(id);
+      if(!existsId)
+      {
+          hashMap.put("Error", true);
+          hashMap.put("Message", "user does not exist with that id");
+          return new ResponseEntity<>
+                  (       hashMap,
+                          HttpStatus.CONFLICT
+                  );
+      }
+        userRepository.deleteById(id);
+        hashMap.put("Message", "user delete");
+        return new ResponseEntity<>
+                (       hashMap,
+                        HttpStatus.ACCEPTED
+                );
+    }
+    }
+
+
 
